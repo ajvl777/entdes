1abc36b1ac348d1ed5750e5192aaafc606fb708f
AntonioJ_Vallinot <a.j.vallinot@gmail.com>
2021-03-06 14:51:34 +0100

    Añadido metodo recibe char y devuelve su representacion decimal.

--- src/ejercicios/metodos.java
+++ src/ejercicios/metodos.java
@@ -1,23 +1,34 @@
 package ejercicios;
 
+import java.io.IOException;
+
 public class metodos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String concatenacion = concatena2Strings("Hola","¿que tal?");
-		System.out.println(concatenacion);
+//		String concatenacion = concatena2Strings("Hola","¿que tal?");
+//		System.out.println(concatenacion);
+//		
+//		System.out.println(recibeCharCorrespondienteAEntero(70));
 		
-		System.out.println(recibeCharCorrespondienteAEntero(70));
+		int caracter = devuelveEnteroCorrespondienteAChar('A');
+		System.out.println(caracter);
 
 	}
 	
 	static String concatena2Strings(String cadena1 , String cadena2) {
+		
 		return cadena1+" "+cadena2;
 	}
 	
 	static char recibeCharCorrespondienteAEntero(int num) {
 		return (char)num;
+	}
+	
+	static int devuelveEnteroCorrespondienteAChar(char caracter) {
+		
+		return caracter;
 	}
 
 }
