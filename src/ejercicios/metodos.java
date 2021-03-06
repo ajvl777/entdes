e1c3d080329075b7ecba157fb092531aeebef32d
AntonioJ_Vallinot <a.j.vallinot@gmail.com>
2021-03-06 14:33:51 +0100

    Añadido metodo que devuelve el char correspondiente a un entero

--- src/ejercicios/metodos.java
+++ src/ejercicios/metodos.java
@@ -7,11 +7,17 @@ public class metodos {
 		
 		String concatenacion = concatena2Strings("Hola","¿que tal?");
 		System.out.println(concatenacion);
+		
+		System.out.println(recibeCharCorrespondienteAEntero(70));
 
 	}
 	
 	static String concatena2Strings(String cadena1 , String cadena2) {
 		return cadena1+" "+cadena2;
+	}
+	
+	static char recibeCharCorrespondienteAEntero(int num) {
+		return (char)num;
 	}
 
 }
