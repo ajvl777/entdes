6f55e7702546e84502f1ce63fee197adbb1261e0
AntonioJ_Vallinot <a.j.vallinot@gmail.com>
2021-03-06 12:33:37 +0100

    Metodo concatenacion de 2 Strings añadido

--- src/ejercicios/metodos.java
+++ src/ejercicios/metodos.java
@@ -4,7 +4,14 @@ public class metodos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String concatenacion = concatena2Strings("Hola","¿que tal?");
+		System.out.println(concatenacion);
 
+	}
+	
+	static String concatena2Strings(String cadena1 , String cadena2) {
+		return cadena1+" "+cadena2;
 	}
 
 }
