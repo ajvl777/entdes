7bed661b1bd9e959dd67ecde5a0f0876932be596
AntonioJ_Vallinot <a.j.vallinot@gmail.com>
2021-03-07 22:07:06 +0100

    Añadido metodo cuenta Atrás

--- src/ejercicios/metodos.java
+++ src/ejercicios/metodos.java
@@ -12,8 +12,10 @@ public class metodos {
 //		
 //		System.out.println(recibeCharCorrespondienteAEntero(70));
 		
-		int caracter = devuelveEnteroCorrespondienteAChar('A');
-		System.out.println(caracter);
+//		int caracter = devuelveEnteroCorrespondienteAChar('A');
+//		System.out.println(caracter);
+		
+		cuentaAtras(20,2);
 
 	}
 	
@@ -29,6 +31,28 @@ public class metodos {
 	static int devuelveEnteroCorrespondienteAChar(char caracter) {
 		
 		return caracter;
+	}
+	
+//	Implementa, dentro de la clase ‘Metodos’, un método que reciba dos variables de tipo int 
+//	como parámetros. El método imprimirá por pantalla una cuenta atrás hasta 0. La primera 
+//	variable indicará el número a partir del cual hay que empezar a contar hacia atrás, mientras 
+//	que la segunda indicará cuantos números hay que restar para la cuenta.
+//	Ejemplo:
+//	Si se llama al método utilizando la siguiente sintaxis: “metodo(51, 5);”
+//	La salida será: 51, 46, 41, 36, 31, 26, 21, 16, 11, 6, 1
+//	
+	
+	static void cuentaAtras(int inicio , int resta) {
+		
+		do {
+			System.out.println(inicio);
+			inicio=inicio-resta;
+			if(inicio<1) {
+				inicio=0;
+				System.out.println(inicio);
+			}
+			
+		}while(inicio !=0);
 	}
 
 }
